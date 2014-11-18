76a41976b7df696f6761eb91938534b4fe4d75eb
OmarElsayed2014 <omar_elsayed_2012@yahoo.com>
2014-11-19 00:09:30 +0200

    Wroking while iam tired at home

--- web/js/custom/deals.js
+++ web/js/custom/deals.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     
     $("#fixed").hide();
     $("#percentage").hide();
+    $("#monthly").hide();
+    $("#poundly").hide();
+    $("#cart-holder-area").hide();
     
    $("input[name=calc-way]").change(function () {
        
@@ -25,6 +28,8 @@ $(document).ready(function () {
             $("#total input").attr('disabled','disabled');
        }
        else {
+            $("#fixed input").val("");
+            $("#percentage input").val("");
             $("#fixed").hide();
             $("#percentage").hide();
             $("#total input").removeAttr('disabled');
@@ -40,7 +45,7 @@ $(document).ready(function () {
        
        var total = price + profit;
        
-       $("#total input").val(total);
+       $("#total input").val(Math.round(total));
    });
    
    $("#percentage input").keyup(function () {
@@ -53,8 +58,63 @@ $(document).ready(function () {
        
        var total = price + (profitPercentage * price / 100);
        
-       $("#total input").val(total);
+       $("#total input").val(Math.round(total));
    });
+   
+    $("#price input").keyup(function () {
+       $("#total input").val("");
+       $("#percentage input").val("");
+       $("#fixed input").val("");
+   });
+   
+   $("input[name=price-way]").change(function () {
+       if($("#total input").val() === "") {
+           alert("لابد من ادخال طريقة حساب الفائدة اولا");
+           $(this).prop( "checked", false );
+           return false;
+       }
+       
+       var calc = $(this).val();
+       
+       if( calc === "fixed") {
+            $("#poundly").show();
+            $("#monthly").hide();
+            $("#monthly input").val("");
+       }
+       else if( calc === "percentage" ) {
+            $("#monthly").show();
+            $("#poundly").hide();
+            $("#poundly input").val("");
+       }
+       else {
+            $("#monthly input").val("");
+            $("#poundly input").val("");
+            $("#monthly").hide();
+            $("#poundly").hide();
+       }
+            
+   });
+   
+   $("#monthly input").keyup(function() {
+       if( !$("#cart-holder").hasClass("animated")) {
+            $("#cart-holder").addClass("animated");
+            $("#cart-holder-area").show();
+            $("#cart-holder").parent().css("cursor", "all-scroll");
+       }
+       
+   });
+   
+   $("#cart-holder").click(function() {
+       if($(this).hasClass("animated")) {
+           $(this).hide();
+           $(this).siblings().hide();
+           
+           showTable();
+       }
+   });
+   
+   
+   
 
 });
 
@@ -64,11 +124,19 @@ function checkEmptyPrice(elem) {
         
         return true;
     }
-    else {
-        if(elem)
-            if($(elem).val().length === 0)
-                return true;
+    
+    if(elem) {
+        if($(elem).val() === "") {
+            $("#total input").val("");
+            return true;
+        }
     }
+}
+
+function showTable() {
+    var holder = $("#cart-holder").parent();
+    
+    $(holder).html("Bokra hkamel isa");
 }
 
 
